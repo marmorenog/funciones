7ae2d04fa4d3dc86f3e243efb6325c516c360326
MarcelaMorenoGajardo <Marcela.Morenog@duocuc.cl>
2026-06-10 11:02:52 -0400

    Materia_de_funciones

---
+++ funcion1_sinparametros.py
@@ -0,0 +1,9 @@
+#función sin parámetros:
+
+def saludar(): #esto es crear o declarar la función definiendola 
+    print("¡Bienvenido al sistema!")
+
+saludar() #ejecutar o invocar una función 
+saludar()
+saludar()
+#bloque de código reutilizable 
---
+++ funcion2_conparametros.py
@@ -0,0 +1,6 @@
+#2. Función con parámetros
+def saludar_usuario(nombre):
+    print(f"¡Hola, {nombre}! Bienvenido.")
+
+saludar_usuario("Ana")
+saludar_usuario("Luis")
